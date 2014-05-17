3d5eff4c0d0f6fb786de0d2f39493827e150d14e
modsky1 <maude.jake@gmail.com>
2014-05-17 10:08:03 -0400

    spacing

--- string_permutation_verification.rb
+++ string_permutation_verification.rb
@@ -1,24 +1,25 @@
+
 def string_permutation_verification(absolute_string,variable_string)
-	if absolute_string.length != variable_string.length
-		puts "#{variable_string} IS NOT a permutation of #{absolute_string}."
+  if absolute_string.length != variable_string.length
+	  puts "#{variable_string} IS NOT a permutation of #{absolute_string}."
 	else
-	count = 0
-	variable_string_array = variable_string.split("")
-	absolute_string_array = absolute_string.split("")
-		variable_string_array.each do |x|
-			absolute_string_array.each do |y|
-				if variable_string_array.each.count(x) == absolute_string_array.each.count(x)
-				else
+	  count = 0
+	  variable_string_array = variable_string.split("")
+	  absolute_string_array = absolute_string.split("")
+	  variable_string_array.each do |x|
+		  absolute_string_array.each do |y|
+			  if variable_string_array.each.count(x) == absolute_string_array.each.count(x)
+			  else
 					count += 1
 				end
 			end
 		end
-		if count > 0
-			puts "#{variable_string} IS NOT a permutation of #{absolute_string}."
-		else
-			puts "#{variable_string} IS a permutation of #{absolute_string}."
-		end
-	end
+	  if count > 0
+		  puts "#{variable_string} IS NOT a permutation of #{absolute_string}."
+	  else
+		  puts "#{variable_string} IS a permutation of #{absolute_string}."
+	  end
+  end
 end
 string_permutation_verification("asdf", "fdsa")
 puts ""
