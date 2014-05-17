da92f61ece25ab4a661311133c4f2c9d8759d256
modsky1 <maude.jake@gmail.com>
2014-05-17 09:53:54 -0400

    spacing

--- string_permutation_verification.rb
+++ string_permutation_verification.rb
@@ -2,25 +2,25 @@ def string_permutation_verification(absolute_string,variable_string)
 	if absolute_string.length != variable_string.length
 		puts "#{variable_string} IS NOT a permutation of #{absolute_string}."
 	else
-	count = 0 
+	count = 0
 	variable_string_array = variable_string.split("")
 	absolute_string_array = absolute_string.split("")
 		variable_string_array.each do |x|
 			absolute_string_array.each do |y|
 				if variable_string_array.each.count(x) == absolute_string_array.each.count(x)
-					
-				else 
+
+				else
 					count += 1
-				end 
-			end 
-		end 
-		if count > 0 
+				end
+			end
+		end
+		if count > 0
 			puts "#{variable_string} IS NOT a permutation of #{absolute_string}."
-		else 
+		else
 			puts "#{variable_string} IS a permutation of #{absolute_string}."
-		end 
-	end 
-end 
+		end
+	end
+end
 string_permutation_verification("asdf", "fdsa")
 puts ""
 string_permutation_verification("asdffd", "fdsa")
